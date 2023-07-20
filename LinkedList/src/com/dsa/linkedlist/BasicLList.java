10f8bfef542e14776f593957e730adb233904b03
Aditya Sharma <aditya1s2h@gmail.com>
2023-07-20 15:03:59 +0530

    LinkedList Reverse Order

--- LinkedList/src/com/dsa/linkedlist/BasicLList.java
+++ LinkedList/src/com/dsa/linkedlist/BasicLList.java
@@ -10,18 +10,42 @@ public class BasicLList {
         }
 
     }
+public static void printList(Node head){
+       if(head == null) return;
+
+    System.out.print(head.data+" ");
+    printList(head.next);
+    }
+
+    public static void print(Node head){
+        while(head!=null){
+            System.out.print(head.data+" ");
+            head = head.next;
+        }
+    }
+
+    public static void printTest(Node head){ // Print in Reverse Order
+        if(head == null) return;
+
+        printTest(head.next);
+        System.out.print(head.data+" ");
+
+    }
 
     public static void main(String[] args) {
+        Node head;
         Node n1 = new Node(5);
         Node n2 = new Node(6);
         Node n3 = new Node(7);
 
         n1.next = n2;
         n2.next = n3;
-
-        while(n1!=null){
-            System.out.print(n1.data+" ");
-            n1 = n1.next;
-        }
+        head = n1;
+        System.out.println("Using Loop");
+        print(head);
+        System.out.println("\nUsing recursion");
+        printList(head);
+        System.out.println("\nUsing recursion TEST");
+        printTest(head);
     }
 }
