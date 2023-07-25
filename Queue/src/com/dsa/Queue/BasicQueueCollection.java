c9bfe7a20e31fe39a6023b595a1403849964df2c
Aditya Sharma <aditya1s2h@gmail.com>
2023-07-25 18:22:56 +0530

    Added Queue Module

---
+++ Queue/src/com/dsa/Queue/BasicQueueCollection.java
@@ -0,0 +1,11 @@
+package com.dsa.Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BasicQueueCollection {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        
+    }
+}
