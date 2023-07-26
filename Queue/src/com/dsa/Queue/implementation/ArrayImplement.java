f1fd1b14bf2c5d71c2c6ff1e4786b677e1703964
Aditya Sharma <aditya1s2h@gmail.com>
2023-07-26 23:46:56 +0530

    ArrayImplementation

---
+++ Queue/src/com/dsa/Queue/implementation/ArrayImplement.java
@@ -0,0 +1,17 @@
+package com.dsa.Queue.implementation;
+
+public class Queue{
+    int f = -1;
+    int r = -1;
+
+    int[] arr= new int[10];
+
+    public void add(int val){
+        
+    }
+}
+public class ArrayImplement {
+    public static void main(String[] args) {
+
+    }
+}
