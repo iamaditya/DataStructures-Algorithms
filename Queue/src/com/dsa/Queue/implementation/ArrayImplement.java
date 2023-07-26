94d5c09b5f6ec569d9ee0158f627b1b2ba9f9eef
Aditya Sharma <aditya1s2h@gmail.com>
2023-07-26 23:59:18 +0530

    Add Remove added

--- Queue/src/com/dsa/Queue/implementation/ArrayImplement.java
+++ Queue/src/com/dsa/Queue/implementation/ArrayImplement.java
@@ -1,13 +1,43 @@
 package com.dsa.Queue.implementation;
 
-public class Queue{
+ class Queue{
     int f = -1;
     int r = -1;
-
+    int size = 0;
     int[] arr= new int[10];
 
     public void add(int val){
-        
+        if(r == arr.length-1){
+            System.out.println("QUEUE IS FULL");
+            return;
+        }
+
+        if(f==-1 && r ==-1){
+            arr[++r] = val;
+            f++;
+        }else{
+            arr[++r] = val;
+        }
+
+        size++;
+    }
+    public int remove(){
+        if(f==-1){
+            System.out.println("QUEUE is Empty");
+            return -1;
+        }
+        int data = arr[f];
+        f++;
+        size--;
+        return data;
+    }
+
+    public int peek(){
+        if(f == -1){
+            System.out.println("QUEUE is Empty");
+            return -1;
+        }
+        return arr[f];
     }
 }
 public class ArrayImplement {
